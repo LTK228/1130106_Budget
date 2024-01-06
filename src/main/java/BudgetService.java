a51f4a414c074d448b12603fb2172969631e3f04
GimmyLee <gimmy40215@yahoo.com.tw>
2024-01-06 16:28:48 +0800

    date is invalid

---
+++ src/main/java/BudgetService.java
@@ -0,0 +1,7 @@
+import java.time.LocalDate;
+
+public class BudgetService {
+    public Double query(LocalDate startDate, LocalDate endDate) {
+        return 0.0;
+    }
+}
--- src/test/java/BudgetTests.java
+++ src/test/java/BudgetTests.java
@@ -1,2 +1,28 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class BudgetTests {
+
+    private BudgetService budgetService;
+
+    @BeforeEach
+    void setUp() {
+        budgetService = new BudgetService();
+    }
+
+    @Test
+    void dateIsInValid() {
+        LocalDate startDate, endDate;
+        startDate = LocalDate.of(2024, 1, 10);
+        endDate = LocalDate.of(2024, 1, 3);
+        totalAmountShouldBe(startDate, endDate, 0.0);
+    }
+
+    private void totalAmountShouldBe(LocalDate startDate, LocalDate endDate, double expected) {
+        assertEquals(expected, budgetService.query(startDate, endDate));
+    }
 }
